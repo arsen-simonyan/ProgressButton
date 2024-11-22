cd75429a7e42cd8c1d58ea61e54417e3744c3ec3
Arsen Simonyan <arsen.simonyan.work@gmail.com>
2024-11-22 18:16:22 +0400

    first commit

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -52,6 +52,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("com.github.arsen-simonyan:ProgressButton:v1.0.0")
 }
 
 afterEvaluate {
